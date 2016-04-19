e9c3d659da87b488548dfd25719194975b844594
Hugo Abonizio <hugo_abonizio@hotmail.com>
2016-04-19 19:48:15 +0000

    Implement file verification

--- lib/spellcop.rb
+++ lib/spellcop.rb
@@ -1,5 +1,28 @@
 require "spellcop/version"
+require 'ffi/hunspell'
 
 module Spellcop
-  # Your code goes here...
+  class FileChecker
+    attr_reader :warnings
+    
+    def initialize(filename)
+      @file = File.read(filename)
+      @warnings = []
+      @dict = FFI::Hunspell.dict('en_US')
+    end
+    
+    def check!
+      comments = @file.scan /#(.*)/
+      comments.each do |comment|
+        words = comment.first.strip.scan /@(\w+)|\[(\w+)| (\w+)/
+        words.each do |result|
+          word = result.compact.first
+          unless @dict.check? word
+#             puts "wrong: #{word}"
+            @warnings << { word: word, suggest: @dict.suggest(word) }
+          end
+        end
+      end
+    end
+  end
 end
